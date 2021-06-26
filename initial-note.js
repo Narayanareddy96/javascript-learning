d4109c546ebbaf268e17fe9cb7135fced82a3717
Narayanareddy96 <gurrala.gvnreddy@gmail.com>
2021-06-26 17:05:40 +0530

    Adding function as a intital start

--- initial-note.js
+++ initial-note.js
@@ -1,1 +1,13 @@
 // this is running notes to track my leaning on advanced concepts
+// summing the n th number from Zero 
+function addNumber_old(n){
+    var total = 0;
+    for(var i=0;i<=n;i++){
+        total +=i;
+    }
+    return total;
+}
+// same function with Math logic 
+function addNumbers_new(n){
+    return n * (n + 1)/2;
+}
